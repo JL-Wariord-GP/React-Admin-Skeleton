b49feb53304b6b790c74fee3fd78569624679385
Jorge Gomez <jorge.luis-leones@hotmail.com>
2023-02-17 21:54:58 -0500

    init

--- src/components/StatBox.jsx
+++ src/components/StatBox.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import { Box, Typography, useTheme } from "@mui/material";
+import { tokens } from "../theme";
+import ProgressCircle from "./ProgressCircle";
 
-const StatBox = () => {
+const StatBox = ({ title, subtitle, icon, progress, increase }) => {
+  
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
 
   return (
     <div>StatBox</div>
