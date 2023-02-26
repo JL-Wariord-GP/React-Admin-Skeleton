9785375ca37ac3c3bf3bad16c578733be702c202
Jorge Gomez <jorge.luis-leones@hotmail.com>
2023-02-25 22:19:13 -0500

    init

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import { ResponsiveLine } from "@nivo/line";
+import { useTheme } from "@mui/material";
+import { tokens } from "../theme";
+import { mockLineData as data } from "../data/mockData";
 
-const LineChart = () => {
-
+const LineChart = ({ isCustomLineColors = false, isDashboard = false }) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
   return (
     <div>LineChart</div>
   )
